40d80b130627e72835d4ffe0f4593a661c76958b
Shashank <shashankjadhav13940@gmail.com>
2024-12-07 23:03:34 +0530

    Matrix done

---
+++ Practical 1/P1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    printf("Enter number of rows and cols: ");
+    scanf("%d%d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < cols; j++)
+            scanf("%d", &matrix[i][j]);
+
+    printf("\n- Matrix -\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++)
+            printf("%d ", matrix[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
