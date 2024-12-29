cbee66c65d2a6fb6a9a40939e7cf94fcdd7eb336
Shashank Jadhav <shashankjadhav13940@gmail.com>
2024-12-29 22:57:13 +0530

    P9 updated

--- Practicals/P9.c
+++ Practicals/P9.c
@@ -2,83 +2,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
+typedef struct Student {
     int rollNo;
     char name[50];
     float percentage;
 } Student;
 
-void bubbleSort(Student students[], int n);
-int binarySearch(Student students[], int size, int rollNo);
-void displayStudent(Student student);
-
 int main() {
-    int n, searchRollNo;
-
+    int n;
     printf("Enter the number of students: ");
     scanf("%d", &n);
+    getchar();
+    Student* student[n];
 
-    Student students[n];
-
-    for (int i = 0; i < n; i++) {
-        printf("\nEnter details for student %d:\n", i + 1);
-        printf("Roll No: ");
-        scanf("%d", &students[i].rollNo);
-        getchar();
+    printf("Enter the students details:\n");
+    for(int i = 0; i < n; i++) {
+        student[i] = (Student*)malloc(sizeof(Student));
+        printf("\nStudent %d\n", i+1);
         printf("Name: ");
-        fgets(students[i].name, sizeof(students[i].name), stdin);
-        students[i].name[strcspn(students[i].name, "\n")] = '\0';
+        fgets(student[i]->name, sizeof(student[i]->name), stdin);
+        student[i]->name[strcspn(student[i]->name, "\n")] = '\0';
+        printf("Roll No.: ");
+        scanf("%d",&student[i]->rollNo);
+        getchar();
         printf("Percentage: ");
-        scanf("%f", &students[i].percentage);
+        scanf("%f", &student[i]->percentage);
+        getchar();
     }
 
-    bubbleSort(students, n);
-
-    printf("\nEnter the Roll No of the student to search: ");
-    scanf("%d", &searchRollNo);
-
-    int index = binarySearch(students, n, searchRollNo);
-
-    if (index != -1) {
-        printf("\nStudent found:\n");
-        displayStudent(students[index]);
-    } else {
-        printf("\nStudent with Roll No %d not found.\n", searchRollNo);
-    }
-
-    return 0;
-}
-
-void bubbleSort(Student students[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1; j++) {
-            if (students[j].rollNo > students[j+1].rollNo) {
-                Student temp = students[j];
-                students[j] = students[j + 1];
-                students[j + 1] = temp;
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(student[j]->rollNo > student[j + 1]->rollNo) {
+                Student* temp = student[j];
+                student[j] = student[j + 1];
+                student[j + 1] = temp;
             }
         }
     }
-}
 
-int binarySearch(Student students[], int size, int rollNo) {
-    int low = 0, high = size - 1;
+    int rollNo;
+    printf("\nEnter roll number to search: ");
+    scanf("%d", &rollNo);
+
+    int low = 0, mid, high = n - 1;
     while(low <= high) {
-        int mid = (low + high) / 2;
-
-        if (students[mid].rollNo == rollNo) {
-            return mid;
-        } else if (students[mid].rollNo < rollNo) {
+        mid = (low + high) / 2;
+        if(student[mid]->rollNo == rollNo) {
+            printf("\nStudent found successfully");
+            printf("\nName: %s", student[mid]->name);
+            printf("\nRoll No.: %d", student[mid]->rollNo);
+            printf("\nPercentage: %.2f", student[mid]->percentage);
+            break;
+        } else if(student[mid]->rollNo < rollNo) {
             low = mid + 1;
         } else {
             high = mid - 1;
         }
     }
-    return -1;
-}
 
-void displayStudent(Student student) {
-    printf("Roll No: %d\n", student.rollNo);
-    printf("Name: %s\n", student.name);
-    printf("Percentage: %.2f\n", student.percentage);
+    printf("\n\n\n- Student List -\n");
+    for(int i = 0; i < n; i++) {
+        printf("Name: %s\n", student[i]->name);
+        printf("Roll No.: %d\n", student[i]->rollNo);
+        printf("Percentage: %.2f\n", student[i]->percentage);
+    }
 }
