a11872b76483ca56f6e83e4ce3d63f9d059ee78c
Shashank Jadhav <shashankjadhav13940@gmail.com>
2024-12-30 01:06:02 +0530

    infix to postfix updated

--- Practicals/P4.c
+++ Practicals/P4.c
@@ -1,51 +1,26 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <math.h>
 
 #define MAX 100
 
-typedef struct {
+typedef struct Stack {
     int top;
     int items[MAX];
 } Stack;
 
-void initStack(Stack *s) { s->top = -1; }
-int isEmpty(Stack *s) { return s->top == -1; }
-int isFull(Stack *s) { return s->top == MAX - 1; }
-
-void push(Stack *s, int element) {
-    if(isFull(s)) {
-        printf("Stack Overflow!\n");
-        return;
-    }
-    s->items[++s->top] = element;
-}
-
-int pop(Stack *s) {
-    if(isEmpty(s)) {
-        printf("Stack Underflow!\n");
-        return;
-    }
-    return s->items[s->top--];
-}
-
-int peek(Stack *s) {
-    if(isEmpty(s)) {
-        printf("Stack is empty!\n");
-        return;
-    }
-    return s->items[s->top];
-}
-
-int precedence(char op) {
-    return (op == '+' || op == '-') ? 1 : (op == '*' || op == '/' || op == '%') ? 2 : (op == '^') ? 3 : 0;
-}
-
+void initStack(Stack* s) { s->top = -1; }
+int isEmpty(Stack* s) { return s->top == -1; }
+int isFull(Stack* s) { return s->top == MAX - 1; }
+void push(Stack* s, int item) { s->items[++s->top] = item; }
+int pop(Stack* s) { return s->items[s->top--]; }
+int peek(Stack* s) { return s->items[s->top]; }
+int precedence(char op) { return (op == '+' || op == '-') ? 1 : (op == '*' || op == '/' || op == '%') ? 2 : (op == '^') ? 3 : 0; }
 int isOperator(char ch) { return strchr("+-*/%^", ch) != NULL; }
 
-void infixToPostfix(const char *infix, char *postfix) {
+void infixToPostfix(const char* infix, char* postfix) {
     Stack s;
     initStack(&s);
     int j = 0;
@@ -55,7 +30,7 @@ void infixToPostfix(const char *infix, char *postfix) {
 
         if(isdigit(ch)) {
             do {
-                postfix[j++] = infix[i++];
+                postfix[j++] = infix[i++]; 
             } while(isdigit(infix[i]));
             postfix[j++] = ' ';
             i--;
@@ -73,29 +48,34 @@ void infixToPostfix(const char *infix, char *postfix) {
             push(&s, ch);
         }
     }
-    while (!isEmpty(&s)) postfix[j++] = pop(&s);
+    while(!isEmpty(&s))
+        postfix[j++] = pop(&s);
     postfix[j] = '\0';
 }
 
-int evaluatePostfix(const char *postfix) {
+int evaluatePostfix(const char* postfix) {
     Stack s;
     initStack(&s);
 
-    for (int i = 0; postfix[i] != '\0'; i++) {
-        if (isdigit(postfix[i])) {
+    for(int i = 0; postfix[i] != '\0'; i++) {
+        if(isdigit(postfix[i])) {
             int num = 0;
-            while (isdigit(postfix[i])) num = num * 10 + (postfix[i++] - '0');
-            push(&s, num);
-        } else if (isOperator(postfix[i])) {
-            int b = pop(&s), a = pop(&s);
-            switch (postfix[i]) {
-                case '+': push(&s, a + b); break;
-                case '-': push(&s, a - b); break;
-                case '*': push(&s, a * b); break;
-                case '/': push(&s, a / b); break;
-                case '%': push(&s, a % b); break;
-                case '^': push(&s, (int)pow(a, b)); break;
+            while(isdigit(postfix[i])) {
+                num = num * 10 + (postfix[i++] - '0');
             }
+            push(&s, num);
+        } else if(isOperator(postfix[i])) {
+            int b = pop(&s), a = pop(&s);
+            switch(postfix[i]) {
+                case '+' : push(&s, a + b); break;
+                case '-' : push(&s, a - b); break;
+                case '*' : push(&s, a * b); break;
+                case '/' : push(&s, a / b); break;
+                case '%' : push(&s, a % b); break;
+                case '^' : push(&s, (int)pow(a,b)); break;
+            }
+        } else if(postfix[i] == ' ') {
+            continue;
         }
     }
     return pop(&s);
@@ -108,9 +88,6 @@ int main() {
 
     infixToPostfix(infix, postfix);
     printf("Postfix Expression: %s\n", postfix);
-
-    int result = evaluatePostfix(postfix);
-    printf("Evaluation Result: %d\n", result);
-
+    printf("Evaluation Result: %d\n", evaluatePostfix(postfix));
     return 0;
 }
